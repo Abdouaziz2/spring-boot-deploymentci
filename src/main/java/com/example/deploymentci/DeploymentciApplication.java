3acea6e0c0070a0541702b3cc4da5a8c14774120
abdouaziz2 <abdouazizseck2015@gmail.com>
2026-04-26 15:28:01 +0000

    update

---
+++ src/main/java/com/example/deploymentci/DeploymentciApplication.java
@@ -0,0 +1,14 @@
+package com.example.deploymentci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentciApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DeploymentciApplication.class, args);
+	}
+
+}
